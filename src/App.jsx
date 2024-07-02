60effdb124d51bc2816bb73ca7d4fe4742dbf66b
LUCAS_ANTONIO_DA_SILVA_DIAS <12200840@OMEGAB36.BARROCA>
2024-07-02 09:04:34 -0300

    js completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import { fetchCBD } from './services/api';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jogos, setJogos] = useState([]);
+
+  useEffect(() => {
+    const carregaJogos = async () => {
+      try {
+        const dadosCDB = await fetchCBD();
+        setJogos(dadosCDB);
+      } catch (error) {
+        console.error("Erro de acesso à API: ", error);
+      }
+    };
+    carregaJogos();
+  }, []);
 
   return (
     <>
@@ -12,11 +24,20 @@ function App() {
       </header>
       <main>
         <div className="container">
-            
+          {jogos.map(jogo => (
+            <div key={jogo.id} className="jogo">
+              <h2>{jogo.ano}</h2>
+              <p>Campeão: {jogo.campeao}</p>
+              <p>Vice: {jogo.vice}</p>
+              <p>Jogo de Ida: {jogo.jogo_ida}</p>
+              <p>Jogo de Volta: {jogo.jogo_volta}</p>
+              <p>Agregado: {jogo.agregado}</p>
+            </div>
+          ))}
         </div>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/services/api.jsx
@@ -0,0 +1,9 @@
+export const fetchCBD = async () => {
+    const response = await fetch('/cdb.json');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data;
+  };
+  
