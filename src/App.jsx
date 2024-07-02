767beb99fb8b57ba8a24fa05553bdfafb905e38a
EDUARDO_FRAGA_FONSECA_GOMES <12300810@OMEGAB37.BARROCA>
2024-07-02 09:17:54 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ function App() {
   return (
     <>
       <header>
-        <p><img src="copa-do-brasil-logo-1.png" alt="" srcset="" /></p>
+        
         <div className="title">
-        <h1 id='title'>Copa do Brasil</h1>
-        <h2 id='subtitle'>aaaaaaaaa</h2>
+        <h1  id='title'> <img src="copa-do-brasil-logo-1.png" alt="" srcset="" />Copa do Brasil</h1>
+        <h2 id='subtitle'>Venha conhecer todos os finalistas de um dos maiores campeonatos do Brasil </h2>
         </div>
         
       </header>
