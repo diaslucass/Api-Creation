c305ed953191c969910f1c11a9d6b5f2fab9de35
LUCAS_ANTONIO_DA_SILVA_DIAS <12200840@OMEGAB36.BARROCA>
2024-07-02 09:45:44 -0300

    maiores vencedores

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,19 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import { fetchCBD } from './services/api';
+import Header from './components/header';
+import Finalistas from './services/finalista';
 
 function App() {
-  const [jogos, setJogos] = useState([]);
-
-  useEffect(() => {
-    const carregaJogos = async () => {
-      try {
-        const dadosCDB = await fetchCBD();
-        setJogos(dadosCDB);
-      } catch (error) {
-        console.error("Erro de acesso à API: ", error);
-      }
-    };
-    carregaJogos();
-  }, []);
+  
 
   return (
     <>
-      <header>
-        
-        <div className="title">
-        <h1  id='title'> <img src="copa-do-brasil-logo-1.png" alt="" srcset="" />Copa do Brasil</h1>
-        <h2 id='subtitle'>Venha conhecer todos os finalistas de um dos maiores campeonatos do Brasil </h2>
-        </div>
-        
-      </header>
+      <Header />
+      <button></button>
       <main>
-        <div className="container">
-
-          {jogos.map(jogo => (
-            <div key={jogo.id} className="jogo">
-              <h2>{jogo.ano}</h2>
-              <p>Campeão: {jogo.campeao}</p>
-              <p>Vice: {jogo.vice}</p>
-              <p>Jogo de Ida: {jogo.jogo_ida}</p>
-              <p>Jogo de Volta: {jogo.jogo_volta}</p>
-              <p>Agregado: {jogo.agregado}</p>
-            </div>
-          ))}
-            
-        </div>
+        <Finalistas />
+        
       </main>
     </>
   );
---
+++ src/components/header.jsx
@@ -0,0 +1,17 @@
+import './header.css'
+
+
+function Header(){
+    return(
+        <header>
+        
+        <div className="title">
+        <h1  id='title'> <img src="copa-do-brasil-logo-1.png" alt="" srcset="" />Copa do Brasil</h1>
+        <h2 id='subtitle'>Venha conhecer todos os finalistas de um dos maiores campeonatos do Brasil </h2>
+        </div>
+        
+      </header>
+    )
+}
+
+export default Header
